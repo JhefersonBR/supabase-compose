48a15530958834e07817200c96cd5bdf8510caca
Jheferson Fonseca Gonçalves <play_jheferson@hotmail.com>
2026-02-26 20:15:58 -0300

    Mais correções sql

--- supabase/code/volumes/db/_supabase.sql
+++ supabase/code/volumes/db/_supabase.sql
@@ -1,7 +1,4 @@
-DO $$
-BEGIN
-  IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = '_supabase') THEN
-    EXECUTE format('CREATE DATABASE %I WITH OWNER %I', '_supabase', current_user);
-  END IF;
-END
-$$;
+SELECT format('CREATE DATABASE %I WITH OWNER %I', '_supabase', current_user)
+WHERE NOT EXISTS (
+  SELECT 1 FROM pg_database WHERE datname = '_supabase'
+)\gexec
