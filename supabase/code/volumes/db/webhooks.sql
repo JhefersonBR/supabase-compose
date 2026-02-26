d479cb0f5629ee914572706516c5fd8a4fa9ab31
Jheferson Fonseca Gonçalves <play_jheferson@hotmail.com>
2026-02-26 20:22:50 -0300

    ajustes

--- supabase/code/volumes/db/webhooks.sql
+++ supabase/code/volumes/db/webhooks.sql
@@ -24,7 +24,7 @@ BEGIN;
   CREATE INDEX IF NOT EXISTS supabase_functions_hooks_request_id_idx ON supabase_functions.hooks USING btree (request_id);
   CREATE INDEX IF NOT EXISTS supabase_functions_hooks_h_table_id_h_name_idx ON supabase_functions.hooks USING btree (hook_table_id, hook_name);
   COMMENT ON TABLE supabase_functions.hooks IS 'Supabase Functions Hooks: Audit trail for triggered hooks.';
-  CREATE FUNCTION supabase_functions.http_request()
+  CREATE OR REPLACE FUNCTION supabase_functions.http_request()
     RETURNS trigger
     LANGUAGE plpgsql
     AS $function$
